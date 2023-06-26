ecc9953285719fc05b5c9f14a4782a9c11147dc1
Orkson <marcinpyta98@gmail.com>
2023-06-26 18:15:32 +0200

    .

--- Startup.cs
+++ Startup.cs
@@ -117,7 +117,6 @@ namespace den_office
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseStaticFiles();
